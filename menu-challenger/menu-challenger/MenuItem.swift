f117ca6625fbcf3ecbbc500d89c4a1c4799b07b1
Alessandro Emanuel <alesefs@hotmail.com>
2024-03-12 16:47:38 -0300

    criação do array do menu

---
+++ menu-challenger/menu-challenger/MenuItem.swift
@@ -0,0 +1,16 @@
+//
+//  MenuItem.swift
+//  menu-challenger
+//
+//  Created by Alessandro Emanuel Ferreira de Souza on 12/03/24.
+//
+
+import Foundation
+
+struct MenuItem {
+    
+    var name: String
+    var price: String
+    var imageName: String
+    
+}
