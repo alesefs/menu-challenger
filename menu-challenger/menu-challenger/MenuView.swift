72c28888202d1060e39ffab7eaf547590856d32b
Alessandro Emanuel <alesefs@hotmail.com>
2024-03-12 16:06:51 -0300

    aula 1 - insere imagens do projeto

---
+++ menu-challenger/menu-challenger/MenuView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  menu-challenger
+//
+//  Created by Alessandro Emanuel Ferreira de Souza on 12/03/24.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MenuView()
+}
---
+++ menu-challenger/menu-challenger/menu_challengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  menu_challengerApp.swift
+//  menu-challenger
+//
+//  Created by Alessandro Emanuel Ferreira de Souza on 12/03/24.
+//
+
+import SwiftUI
+
+@main
+struct menu_challengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MenuView()
+        }
+    }
+}
